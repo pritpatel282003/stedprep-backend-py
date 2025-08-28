b54956543579babcdfcc95821b3815aaa9500cda
pritpatel282003 <pritpatel282003@gmail.com>
2025-08-28 17:05:31 +0530

    sprint planning

--- routes/__init__.py
+++ routes/__init__.py
@@ -5,6 +5,7 @@ from .student_dashboard import router as student_router
 from .study_plan import router as study_plan_router
 from .section_test import router as section_test_router
 from .full_length_test import router as full_length_router
+from .sprint import router as sprint_router
 
 router = APIRouter()
 router.include_router(util_router)
@@ -13,5 +14,4 @@ router.include_router(student_router)
 router.include_router(study_plan_router)
 router.include_router(section_test_router)
 router.include_router(full_length_router)
-
-
+router.include_router(sprint_router)
